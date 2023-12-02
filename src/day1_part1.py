fba1448d6e80e5938b53f7cb3b48989c4f8d7c0f
Jan <schneider.janniklas@gmail.com>
2023-12-03 00:30:56 +0100

    Add devcontainer and day1

---
+++ src/day1_part1.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+import sys
+import re
+
+
+sum = 0
+
+lines = sys.stdin.readlines()
+# for line in sys.stdin:
+for line in lines:
+    if not line:
+        break
+
+    digits = re.findall(r'\d+', line)
+    if digits:
+        if len(digits) == 1:
+            calibration_val = int(digits[0] + digits[0])
+        else:            
+            calibration_val = int(digits[0] + digits[-1])
+        
+        sum += calibration_val
+    
+    if not line:
+        break
+
+print("sum is ", sum)
